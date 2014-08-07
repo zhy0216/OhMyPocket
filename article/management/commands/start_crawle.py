5a5fd49f322083e9c6a52b964c25090151f6d68e
Yang <zhy0216@gmail.com>
2014-08-07 22:44:27 +0800

    make dbnote crawler for testing

--- article/management/commands/start_crawle.py
+++ article/management/commands/start_crawle.py
@@ -1,10 +1,23 @@
 from django.core.management.base import BaseCommand, CommandError
+from pyquery import PyQuery as pq
 
 from utils import redis_conn
+from utils import q
+from article.models import Article, UserPostArticle
+from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     help = 'start crawle stuff'
     def handle(self, *args, **options):
-        ## try http://news.dbanotes.net/
+        machine = User.objects.filter().first()
 
-        pass
+        ## try http://news.dbanotes.net/
+        d = pq(url="http://news.dbanotes.net/")
+        for element in d(".title a"):
+            url = pq(element).attr("href")
+            article, _ = Article.objects.get_or_create(original_url=url)
+            upa, created = UserPostArticle.objects.get_or_create(article=article, user=machine)
+            # post process in rq worker
+            if created:
+                q.enqueue(upa.defer_process)
+
