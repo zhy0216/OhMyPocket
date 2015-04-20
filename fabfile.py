c1d0960c8496591740fecb2ebe96ad82204acd69
Yang <zhy0216@gmail.com>
2015-04-20 22:47:17 +0800

    set up less dev env

---
+++ fabfile.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+
+from fabric.api import local
+from fabric.context_managers import lcd
+
+_warn = logging.warn
+CURRENT_PATH = os.path.join(os.getcwd(),os.path.dirname(__file__))
+
+def cleaning():
+    """Delete all pyc and *.orig files in project directories."""
+    local("find . -name '*.orig' -exec rm -i {} \;")
+    local("find . -type f -name '*.pyc' -exec rm {} \;")
+
+def update_req():
+    """Updating requirements for pip"""
+    # check whether in virtualenv
+    if not os.environ.get("VIRTUAL_ENV"):
+        _warn("You are not in an Virtualenv, please activate it first")
+        return
+    local("pip freeze|grep -v distribute > %s/dev_requirements.txt" % CURRENT_PATH)
+    
+    
+
+    
+    
