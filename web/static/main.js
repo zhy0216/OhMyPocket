8a2b763121961f17707bca587675eb5c28879362
Yang <zhy0216@gmail.com>
2015-04-21 23:01:19 +0800

    finish login stuff

--- web/static/main.js
+++ web/static/main.js
@@ -1,6 +1,59 @@
-$(function(){
+$(function() {
+
+    var switchView = (function(){
+        var curView = null;
+        return function(viewId){
+            function _show(){
+                curView = $('#' + viewId);
+                curView.fadeIn();
+            }
+
+            if(curView){
+                curView.fadeOut(_show);
+            }else{
+                _show();
+            }
+        }
+    })();
+
+    var bindLoginEvent = _.once(function(){
+        $("#login-view .login-btn").on('click', function(){
+            var username = $("#login-view .username").val();
+            var password = $("#login-view .password").val();
+            $.post("/api/account/login", {
+                username: username,
+                password: password,
+            }).done(function(data){
+                console.log(data);
+                router.navigate("random-walk", {trigger: true});
+            });
+
+        })
+    });
+
+    var bindRegisterEvent = _.once(function(){
 
 
-    console.log('start')
+    });
+
+    var router = new (Backbone.Router.extend({
+
+        routes: {
+            "": "",
+            "login": "login",
+            'register': 'register',
+            'article/:articleid': 'showArticle',
+            'random-walk': 'randomWalk',
+        },
+
+        login: function(){
+            switchView('login-view');
+            bindLoginEvent();
+        },
+
+
+    }));
+
+    Backbone.history.start();
 
 });
