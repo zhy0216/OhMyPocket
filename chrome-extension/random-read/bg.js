e11282b174320295df5cb11a9a6bff8538d4aeb9
Yang <zhy0216@gmail.com>
2015-04-23 23:43:30 +0800

    chrome extension has basic functions

--- chrome-extension/random-read/bg.js
+++ chrome-extension/random-read/bg.js
@@ -1,11 +1,5 @@
 var HOST = 'http://localhost:8000';
 
-var is_login = false;
-
-$.post(HOST + "/api/account/check-user-login")
- .done(function(data){
-    is_login = data.is_login;
- })
 
 // add the href directly 
 // if add the article not exsist
@@ -13,10 +7,33 @@ $.post(HOST + "/api/account/check-user-login")
 // we do not need to check whether we login or not
 
 
+// chrome.browserAction.setPopup({popup: "popup.html"});
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-    if(!is_login){
-        chrome.browserAction.setPopup({popup: "popup.html"});
-        return ;
-    }
-    // console.log("is_login: " + is_login);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {action: "require-url"}, function(response) {
+        console.log("response");
+        console.log(response);
+      });
+    });
 });
+
+
+chrome.runtime.onMessage.addListener(function(data){
+    if(data.action == "send-url"){
+        $.post(HOST + "/api/article/add", {
+            url: data.url
+        }).done(function(resp){
+            console.log("add article done");
+            console.log(resp);
+         })
+    }
+});
+
+
+
+
+
+
+
+
---
+++ chrome-extension/random-read/content.js
@@ -0,0 +1,18 @@
+
+
+
+
+function sendUrl(){
+    var url = window.location.href;
+    chrome.runtime.sendMessage({action: 'send-url',
+        url: url
+    });
+}
+
+
+chrome.runtime.onMessage.addListener(function(data) {
+    console.log(data)
+    if(data.action == "require-url"){
+        sendUrl();
+    }
+});
